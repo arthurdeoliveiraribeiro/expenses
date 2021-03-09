e97de557f8eb81f570f5ca5345f307e66924bf91
arthur de oliveira ribeiro <arthuroliveiraribeiro9@gmail.com>
2021-03-09 17:04:34 -0300

    Nova classe Transaction
    Classe nova, parando hoje pq esta dando problema no meu teclado

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 
-
 main() => runApp(ExpensesApp());
+
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage()
-    );
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Versão inicial'),
-      
-    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              child: Text('Grafico'),
+              //CARD
+            ),
+          ),
+          Card(child: Text('Lista de Transações'))
+        ]),
+      );
   }
 }
