6b26cb04e4ba672e21ca29b413717a3a207f28ae
arthur de oliveira ribeiro <arthuroliveiraribeiro9@gmail.com>
2021-03-10 19:06:26 -0300

    Criando lista de transação
    Criando minha lista de transações como exemplo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -10,6 +11,24 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+
+  final _transactions = [
+      Transaction (
+        id: 't1',
+        title: 'Novo Tênis de corrida',
+        value: 310.76,
+        date: DateTime.now()//pegando a data atual
+
+      ),
+      Transaction(
+        id: 't2', 
+        title:'Nova camisa de time' , 
+        value: 150.00,
+        date: DateTime.now()
+        )
+      //Falando que aqui é uma lista de transações
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
