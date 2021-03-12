016fd5d65b5ec9de9d6d905e0e460f00281892d4
arthur de oliveira ribeiro <arthuroliveiraribeiro9@gmail.com>
2021-03-12 17:44:55 -0300

    Parando no meio devido alguns problemas que aconteceu
    Demorei para fazer essa classe pois tive dificuldade de achar o erro

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+
+import 'package:expenses/components/transaction_list.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -12,6 +14,8 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
   final _transactions = [
     Transaction(
         id: 't1',
@@ -44,58 +48,7 @@ class MyHomePage extends StatelessWidget {
                 //CARD
               ),
             ),
-            Column(
-                children: _transactions.map((tr) {
-                //Não importa quantas vezes eu coloque um elemento na lista sempre vai ter mais, pois usei uma função map para aparecer a lista e dar estilo tela, muito massa 
-              return Card(
-                child: Row(children: <Widget>[
-                  Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 2
-                        )
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'R\$ ${tr.value.toStringAsFixed(2)}',//Passando no container o valor e transformando em string
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple
-                        )
-                      )
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,//Colocar no inicio da tela alinhado,
-                    children: <Widget>[
-                      Text(
-                        tr.title,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                      Text(
-                        DateFormat('d MMM y').format(tr.date),
-                        style: TextStyle(
-                          color: Colors.grey[500]
-                        ),
-                        
-                        )
-                    ],
-
-                  )
-                ]),
-                //Passando um card com a lista de titulos das transações
-                //Mapeamento entre as transações que estão no backlog em elementos visuais na tela
-              );
-            }).toList() //Convertendo para uma lista,
-          ),
+          TransactionList(_transactions),
           Card(
             elevation: 5,
             child: Padding(
@@ -103,12 +56,14 @@ class MyHomePage extends StatelessWidget {
               child: Column(
                 children: <Widget>[
                   TextField(
+                    controller: titleController,//pegar o valor que foi passado lá ,
                     decoration: InputDecoration(
                       labelText: 'Título',
                       //O que vai aparecer de informação para o usuário digitar 
                     ),
                   ),
                   TextField(
+                    controller: valueController,
                     decoration: InputDecoration(
                       labelText: 'Valor (R\$)',
                       //O que vai aparecer de informação para o usuário digitar 
@@ -120,9 +75,11 @@ class MyHomePage extends StatelessWidget {
                     child: Text('Nova transação'),
                     color: Colors.blue[50],
                     textColor: Colors.purple[300],
-                    onPressed: (){},
+                    onPressed: (){
+                        print(titleController);
+                        
+                    }                   
                   )
-                  
                 ],
               ),
             ),
